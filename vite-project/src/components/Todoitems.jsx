6d9cce9edb93071bb144bc0c73b2f39027f95933
mariiarode <rodriguezdenizmaria@gmail.com>
2024-12-20 21:42:18 +0000

    finish todo list
    I added a line in the ones that are completed

--- vite-project/src/components/Todoitems.jsx
+++ vite-project/src/components/Todoitems.jsx
@@ -8,7 +8,9 @@ const Todoitems = ({Text, id, isComplete, deleteTodo, toggle}) => {
     <div className='flex items-center my-3 gap-2'>
       <div className='flex flex-1 items-center cursor-pointer'>
         <img onClick={()=>{toggle(id)}} src={isComplete ? tick : not_tick} alt="" className='w-7'/>
-        <p className='text-slate-700 ml-4 text-[17px]'>{Text}</p>
+        <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-800 ${isComplete ? "line-through" : ""}`}>
+            {Text}
+        </p>
       </div>
 
       <img onClick={() => {deleteTodo(id)}} src={delete_icon} alt="" className='w-3.5 cursor-pointer'/>
