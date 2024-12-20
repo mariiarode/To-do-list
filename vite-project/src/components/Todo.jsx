b8b98fb9b7216cf25726d2eb0faa6b841fef3ef6
mariiarode <rodriguezdenizmaria@gmail.com>
2024-12-20 20:42:23 +0000

    Start with the files
    We have started with the background and added the necessary files

---
+++ vite-project/src/components/Todo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div className='bg-white place-self-center w-11/12 max-w-md flex-col p-7 min-h-[550px] rounded-xl'>
+
+        <div className='flex items-center mt-7 gap-2'>
+            <h1>To-Do List</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Todo
