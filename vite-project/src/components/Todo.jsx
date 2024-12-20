1f68613f9d5adeda7dd69c55fb9c74c4486b80ad
mariiarode <rodriguezdenizmaria@gmail.com>
2024-12-20 21:17:35 +0000

    Create the list
    Here we have programmed the to-do list that will be displayed

--- vite-project/src/components/Todo.jsx
+++ vite-project/src/components/Todo.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import todo_icon from '../assets/todo_icon.png'
+import Todoitems from './Todoitems'
 
 const Todo = () => {
+
+const [todoList, setTodoList] = useState([]);
+const inputRef = useRef();
+
+
+const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === ""){
+        return null;
+    }
+
+    const newTodo = {
+        id: Date.now(),
+        text: inputText,
+        isComplete: false,
+    }
+    setTodoList((prev)=> [...prev, newTodo]);
+    inputRef.current.value="";
+}
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex-col p-7 min-h-[550px] rounded-xl'>
 
         <div className='flex items-center mt-7 gap-2'>
-            <h1>To-Do List</h1>
+            <img className='w-8' src={todo_icon} alt="" />
+            <h1 className='text-3xl font-semibold'>To-Do List</h1>
         </div>
       
+        <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
+            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD</button>
+        </div>
+
+        <div>
+
+            {todoList.map((item, index)=>{
+                return <Todoitems key={index} Text={item.text}/>
+            })}
+
+        </div>
     </div>
   )
 }
